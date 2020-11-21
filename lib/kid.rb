9715a43e24fb57c8ca22cf53d059abf540b6cd56
okorkov <alex.okorkov@gmail.com>
2020-11-20 20:31:24 -0800

    commit tree 0c0c433f456d61dc25fd2da3e56c29b204d76929

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+
+class Kid
+
+    extend MetaDancing
+        # extend FancyDance::ClassMethods
+        # include FancyDance::InstanceMethods
+        include Dance
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+end 
